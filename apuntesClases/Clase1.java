084dee6fa13715a114b1f1f19a842c4b98cf45ad
t3rma <t3rma1234@gmail.com>
2024-03-12 10:07:45 -0300

    Primer Commit, primera clase

---
+++ apuntesClases/Clase1.java
@@ -0,0 +1,91 @@
+
+public class Clase1{
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Temas centrales de la materia:
+		 * 
+		 * - Algoritmia
+		 * 
+		 * - Tipos de datos abstractos
+		 * 
+		 * 
+		 * 	Algoritmo: La estrategia a implementar para la resolucion de un problema
+		 * 
+		 * 	Algoritmia: Analisis, Diseño e Implementacion de algoritmos
+		 * 
+		 * 	Programa: Colleciones de algoritmos
+		 * 
+		 * 	Programa =/= Algoritmo
+		 * 
+		 * 	(Vamos a volver con algoritmos cuando analizemos costos y complejidad)
+		 * 
+		 * 
+		 * 	-Tipos de datos abstractos:
+		 * 	
+		 * Primitivos:
+		 * 	Tipo de dato = {Dominio, Operaciones}
+		 * 	Primitivos(java): byte, short, int, long, String, char, double, float, bool
+		 * 	
+		 * 	Mecanismos de estructuracion de tipos de datos:
+		 * 
+		 * 	Arreglos -> Componentes homogeneos
+		 * 
+		 * 	Registros -> Componenetes heterogeneos
+		 * 
+		 * Abstractos:
+		 * 	Clases, objetos, etc.
+		 * 
+		 * 	Especificaciones:
+		 * 		
+		 * 		Informales: -Lenguaje natural, (falta de precision)
+		 * 					-Diagramas
+		 * 					Ventajas: facilidad de comprension, Flexibilidad, Rapidez
+		 * 
+		 * 		Formales: -Permite expresar sin ambiguedad, de manera rigurosa
+		 * 				  -Ventajas: precision, claridad, rigurosidad, modularidad
+		 * 	
+		 * 		Abstraccion:
+		 * 			Representa tanto la funcionalidad como la composicion del objeto
+		 * 			del cual se analiza y cuenta con niveles del mismo.
+		 * 			Depende del usuario
+		 * 			
+		 * 
+		 * */
+		
+		/*TDA's
+		 * 
+		 * Pilas: almacena valores, eliminarlos, recuperarlos, con el comportamiento
+		 * LIFO (Last In, First Out)
+		 * 
+		 * Conjuntos minimos de operaciones para definir TDAs:
+		 * -Constructoras Basicas
+		 * -Observadoras 
+		 * -Modificadoras
+		 * 
+		 * Constructoras basicas: operaciones que permite los valores del tipo, desde
+		 * la pila vacia a la pila de n elementos
+		 * 
+		 * Observadoras: operaciones que nos permite obtener informacion de la pila
+		 * sin modificarla
+		 * 
+		 * Modificadores -
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		/*JAVA
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+		
+	}
+
+}
